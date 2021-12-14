536eb9942c44b5fb596e0004c12a9fd4ffe9d96e
vadyaChmil <zakusylo.vadym@gmail.com>
2021-12-15 00:20:01 +0200

    spring-mvc-01

---
+++ src/main/java/com/luxoft/springmvc01/SpringMvc01Application.java
@@ -0,0 +1,13 @@
+package com.luxoft.springmvc01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvc01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/luxoft/springmvc01/SpringMvc01ApplicationTests.java
@@ -0,0 +1,22 @@
+package com.luxoft.springmvc01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SpringMvc01ApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	public void exampleTest() {
+		String body = this.restTemplate.getForObject("/", String.class);
+	}
+}
