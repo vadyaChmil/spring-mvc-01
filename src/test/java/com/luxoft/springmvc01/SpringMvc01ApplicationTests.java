b40e0c54a4432f5404985590bbf0d073f80140fb
vadyaChmil <zakusylo.vadym@gmail.com>
2021-12-15 08:57:46 +0200

    15.12.2021

--- src/test/java/com/luxoft/springmvc01/SpringMvc01ApplicationTests.java
+++ src/test/java/com/luxoft/springmvc01/SpringMvc01ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.luxoft.springmvc01;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,5 +19,6 @@ class SpringMvc01ApplicationTests {
 	@Test
 	public void exampleTest() {
 		String body = this.restTemplate.getForObject("/", String.class);
+		Assert.assertTrue(body.contains("Hello World"));
 	}
 }
